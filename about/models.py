6cb97db9ec198a52530377e3c54e0b2336636669
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-04-20 10:05:10 +0000

    Add minor changes to signout and about

--- about/models.py
+++ about/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class AboutUs(models.Model):
     restaurant_name = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='about_us', blank=True)
+
+    class Meta:
+        verbose_name = 'about us'
+        verbose_name_plural = 'about us'
