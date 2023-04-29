acc073156981a304b53e80761ec258f55393ff11
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-04-29 15:04:37 +0000

    Add

--- table_booking/forms.py
+++ table_booking/forms.py
@@ -2,11 +2,24 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import Booking
 from .widget import DatePickerInput, TimePickerInput
-
 from django.utils import timezone
+from datetime import time
+
+
+def validate_opening_hours(value):
+    opening_time = time(14, 0)  # replace with your opening time
+    closing_time = time(23, 0)  # replace with your closing time
+    last_booking_time = closing_time.replace(hour=closing_time.hour-2, minute=0, second=0, microsecond=0)
+
+    if not (opening_time <= value <= closing_time):
+        raise forms.ValidationError("Please select a time within opening hours (2pm - 11pm).")
+    elif value > last_booking_time:
+        raise forms.ValidationError("Sorry, the last online booking can be made at most 2 hours before closing time.")
 
 
 class BookingTableForm(forms.ModelForm):
+    reservation_time = forms.TimeField(widget=TimePickerInput(attrs={'class': 'form-control'}), validators=[validate_opening_hours])
+
     class Meta:
         model = Booking
         fields = ('name', 'email', 'phone', 'number_of_guests', 'reservation_date', 'reservation_time', 'notes')
@@ -16,7 +29,7 @@ class BookingTableForm(forms.ModelForm):
             'phone': forms.TextInput(attrs={'placeholder': 'Enter your phone number'}),
             'number_of_guests': forms.NumberInput(attrs={'min': 1, 'max': 8}),
             'reservation_date': DatePickerInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'reservation_time': TimePickerInput(attrs={'class': 'form-control'}),
+            # 'reservation_time': TimePickerInput(attrs={'class': 'form-control'}),
             'notes': forms.Textarea(attrs={'rows': 2, 'placeholder': 'Enter any special request'}),
         }
 
