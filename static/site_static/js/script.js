47582595edc4249b61949f950f1cded6620afe69
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-04-30 18:00:11 +0000

    Add

--- static/site_static/js/script.js
+++ static/site_static/js/script.js
@@ -1,25 +1,19 @@
 // Messages
 
 setTimeout(function () {
-    document.getElementById('msg').classList.add('hide');
-}, 10000);
+  document.getElementById('msg').classList.add('hide');
+}, 8000);
 
 
 // Home page Menu tabs
 
-// setTimeout(function () {
-//     let messages = document.getElementById('msg');
-//     let alert = new bootstrap.Alert(messages);
-//     alert.close();
-//   }, 2500);
+$(document).ready(function () {
+  // Activate the first tab by default
+  $('#pills-tab a:first').tab('show');
 
-  $(document).ready(function () {
-    // Activate the first tab by default
-    $('#pills-tab a:first').tab('show');
-
-    // Activate the selected tab on click
-    $('#pills-tab a').click(function (e) {
-      e.preventDefault();
-      $(this).tab('show');
-    });
+  // Activate the selected tab on click
+  $('#pills-tab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
   });
+});
